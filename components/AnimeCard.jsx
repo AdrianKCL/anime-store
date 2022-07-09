4a325f758a806af0a315f3e3a21660f4ed436dd8
AdrianKCL <92375484+AdrianKCL@users.noreply.github.com>
2022-07-09 23:17:41 +0200

    pls work

--- components/AnimeCard.jsx
+++ components/AnimeCard.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function AnimeCard({ anime }) {
+  const navigate = useNavigate();
   return (
     <div className="anime__wrapper">
       <div className="anime__card">
@@ -9,9 +11,14 @@ export default function AnimeCard({ anime }) {
         </figure>
         <div className="anime__description">
           <div className="anime__title">{anime.title}</div>
-          <Link to={`/details/${anime.mal_id}`}>
-            <button className="anime__button">See details</button>
-          </Link>
+          {/* <Link to={`/details/${anime.mal_id}`}> */}
+          <button
+            className="anime__button"
+            onClick={`/details/${anime.mal_id}`}
+          >
+            See details
+          </button>
+          {/* </Link> */}
         </div>
       </div>
     </div>
